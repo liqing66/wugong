3d601dd6314561b75ebdf3aa838aa4eaa2867935
liqing <liqing@qq.com>
2018-03-20 20:33:43 +0800

    增加了7899

--- src/com/atguigu/spring/dugujiujian.java
+++ src/com/atguigu/spring/dugujiujian.java
@@ -8,6 +8,10 @@ public class dugujiujian {
 		System.out.println("444444");
 		System.out.println("333");
 		System.out.println("5555");
+		System.out.println("6666");
+		System.out.println("7777");
+		System.out.println("8888");
+		System.out.println("999999");
        
 		
 	}
