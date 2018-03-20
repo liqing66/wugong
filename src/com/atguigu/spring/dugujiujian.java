b0aa2e8e324e0432f50f74b968ce51681c43e011
liqing <liqing@qq.com>
2018-03-20 20:16:31 +0800

    剑法

---
+++ src/com/atguigu/spring/dugujiujian.java
@@ -0,0 +1,15 @@
+package com.atguigu.spring;
+
+public class dugujiujian {
+
+	public static void main(String[] args) {
+		System.out.println("1111");
+		System.out.println("22222");
+		System.out.println("444444");
+		System.out.println("333");
+       
+		
+	}
+
+
+}
